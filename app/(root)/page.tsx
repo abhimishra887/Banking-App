237d0bfbf8aed4cf2abea8a9f23c38b0715c0ab1
abhimishra887 <abhishek42387@gmail.com>
2024-08-22 18:38:43 +0530

    implement sidebar and menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Abhishek', lastName: 'Mishra', email: 'abhishek42387@gmail.com'};
   return (
     <div>
-      Home
+      <section className="home">
+        <div className="home-content">
+          <header className="home-header">
+            <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transaction efficiently."
+            />
+
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={2450.45}
+            />
+          </header>
+
+          RECENT TRANSACTION 
+        </div>
+
+        <RightSidebar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{},{}]}
+        />
+      </section>
     </div>
   )
 }
